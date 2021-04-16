564cb4618699b84f491b25ac33551a8c0961d2ee
giulia matos serpa <79885136+giuliamatos@users.noreply.github.com>
2021-04-16 00:14:34 -0300

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+</head>
+<body>
+
+<h1>A Web Page</h1>
+<p id="demo">A Paragraph</p>
+<button type="button" onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+
+<script src="https://www.w3schools.com/js/myScript1.js"></script>
+
+</body>
+</html>
